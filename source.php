ed38c5f8e1c04912c3f932cbe812e3eccb77111b
Dʀ.Kei1 <80795572+DrKei1@users.noreply.github.com>
2022-06-05 03:08:29 +0200

    Create source.php

---
+++ source.php
@@ -0,0 +1,3 @@
+<?php
+echo "http://sharifteam.ir/downloads/%d8%b3%d9%88%d8%b1%d8%b3-%d8%af%d8%a7%d9%86%d9%84%d9%88%d8%af-%d9%85%d8%b3%d8%aa%d9%82%db%8c%d9%85-%d8%a7%d8%b2-%da%a9%d8%a7%d9%81%d9%87-%d8%a8%d8%a7%d8%b2%d8%a7%d8%b1/";
+?>
